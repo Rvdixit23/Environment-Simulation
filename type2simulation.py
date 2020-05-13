a39337db0b1c608d6533a01eab196a7f08e269c7
Rvdixit23 <rvdixit23@gmail.com>
2020-05-13 13:35:33 +0530

    Added explationation for the working of type2simulation

--- type2simulation.py
+++ type2simulation.py
@@ -4,8 +4,13 @@ import random
 import math
 
 
-def gauss_random():
-    return abs(1 - abs(random.gauss(0, 13 / 45)))
+def gauss_random(mean = 0, variance = 13/45):
+    """
+    Wrapper function for generating random numbers of the desired distribution
+    Parameters : mean, variance
+    Returns : Random number sampled from Gaussian Distribution
+    """
+    return abs(1 - abs(random.gauss(mean, variance)))
 
 
 def exp_random():
@@ -55,9 +60,9 @@ f = 10  # number of features
 n = 100  # initial number of beings
 per = 50  # lower percentage of population
 adapt_per = 25  # chance to mutate to an advanced specie
-epochs = 1000  # number of generations
-food_lower=80 #least amount of food
-food_higher=120 #most amount of food
+epochs = 200  # number of generations
+food_lower = 80  # least amount of food
+food_higher = 120  # most amount of food
 
 qualities = list(
     set([tuple([random.randint(2, 8) for i in range(f)]) for i in range(50)]))
@@ -70,21 +75,21 @@ print(qualities_dict)
 print("\n\n")
 print("initial_beings")
 print(populist)
-print("\n\n") 
+print("\n\n")
 
 food_list = [random.randint(food_lower, food_higher)
-             for i in range(epochs)]  #resource available for each generation
+             for i in range(epochs)]  # resource available for each generation
 
 for i in range(epochs):  # number of epochs (generations)
-    if(not int (i%100)):
+    if(not int(i % 100)):
         print("running epoch_set:", i)
         print(populist)
-        print("\n\n") 
+        print("\n\n")
     populmatrix = np.array([qualities_dict[i] for i in populist])
     result = populmatrix.dot(chance)
     result_norm = np.asarray(
         np.interp(result, (result.min(), result.max()), (0, 1))).reshape(-1)
-    #print(result_norm)
+    # print(result_norm)
     new_populist = [0]
     for j in range(food_list[i]):
         new_birth = food_lucky(result_norm, populist)
