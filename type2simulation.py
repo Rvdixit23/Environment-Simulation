f5000638b47317d7765216de808bc2c6a32d5be6
Rvdixit23 <rvdixit23@gmail.com>
2020-05-15 17:49:27 +0530

    Added explanations

--- type2simulation.py
+++ type2simulation.py
@@ -18,12 +18,26 @@ def gauss_random(mean=0, variance=13 / 45):
 
 
 def generate_qualdict(qualities, chance):
+    """
+    This function returns the dictionary of the different species
+    Keys of the dictionary shows how good the species is
+    Example : qualDict[0] is inferior to qualDict[10]
+    Input : List of tuples containing the rating for each feature of the species
+    Output : Dictionary as specified above
+    """
     qualities = sorted(qualities, key=lambda x: np.array(x).dot(chance))
     qualities.insert(0, [0 for i in range(len(qualities[1]))])
     return dict(zip(range(len(qualities)), qualities))
 
 
 def init_population_generate(qualities_dict, num, lower_percentage):
+    """
+    This creates a list of beings
+    Each being is referred to by the species number as specified in qualities_dict
+    num : Number of beings in the population
+    lower_percentage : Percentage of population belonging to inferior species
+    Returns : List with species number for num beings
+    """
     max_key = int(max(qualities_dict.keys()) * lower_percentage / 100)
     populist = []
     for i in range(num):
@@ -33,6 +47,14 @@ def init_population_generate(qualities_dict, num, lower_percentage):
 
 
 def food_lucky(arr, populist):
+    """
+    This function controls mutation within species
+    val : Original species index
+    qualities_dict : The species data
+    chance : The probability of survival
+    adapt_percent : The probability of mutation
+    Returns : The new species index of the original species
+    """
     minimum = 10000
     ind = -1
     while (ind == -1 or populist[ind] == 0):
@@ -66,12 +88,16 @@ food_lower = 180  # least amount of food
 food_higher = 360  # most amount of food
 types_of_beings = 50  # max types of beings
 
+# The different types of species in the system
 qualities = []
 while (len(qualities) != types_of_beings):
     ele = [random.randint(2, 8) for i in range(f)]
+    # Each of them have 'f' features and how much a species excels in that feature
+    # is given by a number from 1 to 10
     if (ele not in qualities):
         qualities.append(ele)
 
+# Chance matrix decides how much a feature influences the survival chance of that being
 chance = np.reshape([random.randint(3, 6) for i in range(f)], (f, 1))
 qualities_dict = generate_qualdict(qualities, chance)
 populist = init_population_generate(qualities_dict, n, per)
@@ -94,14 +120,29 @@ for i in range(epochs):  # number of epochs (generations)
         print("running epoch_set:", i)
         print(sorted(populist))
         print("\n\n")
+        # Prints the population for every 100 epochs
+
+    # populmatrix is the array of lists with each list containing the features of the species
+    # that the being belongs to
     populmatrix = np.array([qualities_dict[i] for i in populist])
+    
+    # The produce of the chance and population matrix gives the array of the chance of survival
     result = populmatrix.dot(chance)
+    
+    # The results of the probability is normalized to be in between 0 and 1 to use RNG to 
+    # Decide the survival of that being
     result_norm = np.asarray(
         np.interp(result, (result.min(), result.max()), (0, 1))).reshape(-1)
     # print(result_norm)
     new_populist = [0]
     for j in range(food_list[i]):
+        # For each food item on the ith day
+
+        # Food lucky decides whether the each being in the population gets food
+        # In that particular epoch
         new_birth = food_lucky(result_norm, populist)
+        
+        # The new_populist contains the modified species of the previous population
         new_populist.append(
             mutator(new_birth, qualities_dict, chance, mutate_per))
     populist = new_populist
